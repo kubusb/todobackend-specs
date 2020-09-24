0bd602a249ef5a4bf2d24fc4059e4c5f0abb2606
Jakub Porebski <kubusb@gmail.com>
2020-09-24 10:10:54 +0200

    Helper functions added

---
+++ test/tests.js
@@ -0,0 +1,47 @@
+var chai = require('chai'),
+    should = chai.should,
+    expect = chai.expect,
+    Promise = require('bluebird'),
+    request = require('superagent-promise')(require('superagent'), Promise),
+    chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+var url = processes.env.URL || 'http://localhost:8000/todos';
+
+/*
+ * Convinience functions
+ */
+
+// POST request with data and return promise
+funcion post(url, data) {
+  return request.post(url)
+    .set('Content-Type', 'application/json')
+    .set('Accept', 'application/json')
+    .send(data)
+    .end;
+ }
+
+// GET request and return promise
+function get(url) {
+  return request.get(url)
+    .set('Accept', 'application/json')
+    .end();
+}
+
+// DELETE request and return promise
+function del(url) {
+  return request.del(url).end();
+}
+
+// UPDATE request with dataand return promise
+function update(url, method, data) {
+  return request(method, url)
+    .set('Content-Type', 'application/json')
+    .set('Accept', 'application/json')
+    .send(data)
+    .end();
+}
+
+// Resolve promise for property and return expectation
+function assert(result, prop) {
+  return expect(result).to.eventually.have.deep.property(prop)
+}
